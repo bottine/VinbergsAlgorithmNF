4d4f7be89e97153e346664912597dd3980830ecf
wesh <wesh@fake.local>
2021-04-03 08:24:34 +0200

    more easier lattices management

--- src/some_lattices.jl
+++ src/some_lattices.jl
@@ -3,7 +3,6 @@
 Compute the "block diagonal" matrix with blocks ``M₁`` and ``M₂``, assuming both are square symetric.
 """
 function ⊕(M₁,M₂)
-    
 
     n₁ = size(M₁)[1]
     n₂ = size(M₂)[1]
@@ -17,54 +16,65 @@ function ⊕(M₁,M₂)
     elseif n₁ == 0
         return M₂
     else
-        K₁ = parent(M₁[1,1])
-        K₂ = parent(M₂[1,1])
-        @assert K₁ == K₂
-
-        P = K₁.(zeros(Int,n₁+n₂,n₁+n₂))
+        P = Array{Any, 2}(undef, n₁+n₂, n₁+n₂)
+        P[:,:] = fill(0,n₁+n₂,n₁+n₂)
         P[1:n₁,1:n₁] = M₁
         P[n₁+1:end,n₁+1:end] = M₂
 
         return P
     end
+end
+
+U_() = [0 1; 1 0]
+A_(n) = Matrix(LinearAlgebra.SymTridiagonal([2 for i in 1:n], [-1 for i in 1:n-1]))
+I_(n) = Matrix(LinearAlgebra.I(n))
+D_(n) = begin
+    @assert n≥4
+    M = A_(n)
+    M[end,end-1] = 0
+    M[end-1,end] = 0
+    M[end,end-2] = -1
+    M[end-2,end] = -1
+    return M
+end
+
+E_(n) = begin
+    if n==6
+        return [ 
+            2  0 -1  0  0  0;
+            0  2  0 -1  0  0;
+            -1  0  2 -1  0  0;
+            0 -1 -1  2 -1  0;
+            0  0  0 -1  2 -1;
+            0  0  0  0 -1  2
+        ]
+    elseif n == 7
+        return [
+            2  0 -1  0  0  0  0;
+             0  2  0 -1  0  0  0;
+            -1  0  2 -1  0  0  0;
+             0 -1 -1  2 -1  0  0;
+             0  0  0 -1  2 -1  0;
+             0  0  0  0 -1  2 -1;
+             0  0  0  0  0 -1  2
+        ]
+    elseif n == 8
+        return [ 
+        2  0 -1  0  0  0  0  0;
+         0  2  0 -1  0  0  0  0;
+        -1  0  2 -1  0  0  0  0;
+         0 -1 -1  2 -1  0  0  0;
+         0  0  0 -1  2 -1  0  0;
+         0  0  0  0 -1  2 -1  0;
+         0  0  0  0  0 -1  2 -1;
+         0  0  0  0  0  0 -1  2
+        ] 
+    end
+    @assert false "Invalid number"
 
 end
 
-U_ = [0 1; 1 0]
-A_(n) = Matrix(LinearAlgebra.SymTridiagonal([2 for i in 1:n], [1 for i in 1:n-1]))
-I_ = Matrix(LinearAlgebra.I(1))
 
-
-
-gram_E8 =[ 
-2  0 -1  0  0  0  0  0;
- 0  2  0 -1  0  0  0  0;
--1  0  2 -1  0  0  0  0;
- 0 -1 -1  2 -1  0  0  0;
- 0  0  0 -1  2 -1  0  0;
- 0  0  0  0 -1  2 -1  0;
- 0  0  0  0  0 -1  2 -1;
- 0  0  0  0  0  0 -1  2
-] 
-
-gram_E7 = [
-    2  0 -1  0  0  0  0;
-     0  2  0 -1  0  0  0;
-    -1  0  2 -1  0  0  0;
-     0 -1 -1  2 -1  0  0;
-     0  0  0 -1  2 -1  0;
-     0  0  0  0 -1  2 -1;
-     0  0  0  0  0 -1  2
-]
-
-gram_E6 = [ 
-    2  0 -1  0  0  0;
-    0  2  0 -1  0  0;
-    -1  0  2 -1  0  0;
-    0 -1 -1  2 -1  0;
-    0  0  0 -1  2 -1;
-    0  0  0  0 -1  2
-]
 
 # Can't do this here: https://github.com/Nemocas/Nemo.jl/issues/810
 #Krat,arat = Hecke.rationals_as_number_field()
